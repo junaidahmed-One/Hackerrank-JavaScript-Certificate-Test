56abd62f6b4cdef04e3c6cfeece3edd39e4765e1
junaidahmed-One <junaidahmed882000@gmail.com>
2023-11-27 15:28:24 +0530

    Hackerrank JS Basics Test

---
+++ dataFinder.js
@@ -0,0 +1,18 @@
+
+const dataFinder = (data) => {
+    return function find(minRange,maxRange,value){
+        if(maxRange >= data.length){
+            throw new Error("Invalid range");
+        }
+        for(let i=minRange;i<=maxRange;i++){
+            if(data[i] === value){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+const ans = dataFinder([1,2,3,0,7,22,1]);
+
+console.log(ans(2,100,22));
---
+++ notesStore.js
@@ -0,0 +1,49 @@
+const notes = {
+    states:{
+        active:[],
+        completed:[],
+        others:[]
+    }
+}
+
+class NotesStore {
+    
+    addNote(state,name){
+        if(name === null){
+            throw new Error("Name cannot be empty");
+        }else if(state === 'active'){
+            notes.states.active.push(name);
+        }
+        else if(state === 'completed'){
+            notes.states.completed.push(name);
+        }else if(state === 'others'){
+            notes.states.completed.push(name);
+        }
+        else if(state !== 'active' || state !== 'completed' ){
+            throw new Error(`Invalid state ${state}`);
+        }
+        
+    }
+    
+    getNotes(state){
+        if(state === 'active'){
+            return notes.states.active;
+        }else if(state === 'completed'){
+            return notes.states.completed;
+        }else if(state === 'others'){
+            return notes.states.others;
+        }
+        else if(state !== 'active' || state !== 'completed' ){
+            throw new Error(`Invalid state ${state}`);
+        }
+    }
+    
+}
+
+const obj = new NotesStore();
+
+obj.addNote('active','DrinkTea');
+obj.addNote('active','DrinkCoffe');
+obj.addNote('completed','Study');
+console.log(obj.getNotes('active'));
+console.log(obj.getNotes('completed'));
