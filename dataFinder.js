319943699add5f6b93069040c6be5f3801cee156
junaidahmed-One <junaidahmed882000@gmail.com>
2023-11-28 15:02:22 +0530

    Javascript advance certificate test done

--- dataFinder.js
+++ dataFinder.js
@@ -1,18 +1,17 @@
-
 const dataFinder = (data) => {
-    return function find(minRange,maxRange,value){
-        if(maxRange >= data.length){
-            throw new Error("Invalid range");
-        }
-        for(let i=minRange;i<=maxRange;i++){
-            if(data[i] === value){
-                return true;
-            }
-        }
-        return false;
+  return function find(minRange, maxRange, value) {
+    if (maxRange >= data.length) {
+      throw new Error("Invalid range");
     }
-}
+    for (let i = minRange; i <= maxRange; i++) {
+      if (data[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  };
+};
 
-const ans = dataFinder([1,2,3,0,7,22,1]);
+const ans = dataFinder([1, 2, 3, 0, 7, 22, 1]);
 
-console.log(ans(2,100,22));
+console.log(ans(2, 100, 22));
---
+++ employeeInheritance.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (chunk) {
+  inputString += chunk;
+});
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+function Employee(title) {
+  this.title = title;
+  this.setTitle = function (newTitle) {
+    this.title = newTitle;
+  };
+
+  this.getTitle = function () {
+    return this.title;
+  };
+}
+
+Employee.prototype.setTitle = function (newTitle) {
+  this.title = newTitle;
+};
+Employee.prototype.getTitle = function () {
+  return this.title;
+};
+
+function Engineer(title, isManager) {
+  Employee.call(this, title);
+  this.isManager = isManager;
+
+  this.setIsManager = function (isman) {
+    this.isManager = isman;
+  };
+
+  this.getIsManager = function () {
+    return this.isManager;
+  };
+}
+
+Engineer.prototype.setIsManager = function (isman) {
+  this.isManager = isman;
+};
+
+Engineer.prototype.getIsManager = function () {
+  return this.isManager;
+};
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  var inputs = readLine().split(" ");
+  var engineerObject = new Engineer(
+    inputs[0],
+    inputs[1].toLowerCase() === "true"
+  );
+
+  ws.write(
+    `Initial Employee Profile - Title is ${engineerObject.getTitle()}. ${
+      engineerObject.getIsManager() ? "Is" : "Is not"
+    } a Manager\n`
+  );
+
+  engineerObject.setTitle(readLine());
+  engineerObject.setIsManager(readLine().toLowerCase() === "true");
+
+  ws.write(
+    `Final Employee Profile - Title is ${engineerObject.getTitle()}. ${
+      engineerObject.getIsManager() ? "Is" : "Is not"
+    } a Manager\n`
+  );
+
+  ws.write(
+    `Engineer.prototype has property setTitle: ${Engineer.prototype.hasOwnProperty(
+      "setTitle"
+    )}\n`
+  );
+  ws.write(
+    `Engineer.prototype has property getTitle: ${Engineer.prototype.hasOwnProperty(
+      "getTitle"
+    )}\n`
+  );
+  ws.write(
+    `Engineer.prototype has property setIsManager: ${Engineer.prototype.hasOwnProperty(
+      "setIsManager"
+    )}\n`
+  );
+  ws.write(
+    `Engineer.prototype has property getIsManager: ${Engineer.prototype.hasOwnProperty(
+      "getIsManager"
+    )}\n`
+  );
+}
